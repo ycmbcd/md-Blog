4422811ae20dc8a95520a61a514bd131d9783255
ycmbcd <ycmbcd@foxmail.com>
2020-05-13 13:51:59 +0800

    42-UI调整+新增右侧目录悬浮按钮

--- static/js/blog.js
+++ static/js/blog.js
@@ -77,6 +77,7 @@ $(function(){
     if(right_nav !== ''){
         right_nav = '<br><div id="right_nav" class="b-box boxes">' + right_nav + '</div>';
         setTimeout(function(){
+            $('.fix_content_html').html(right_nav)
             // 追加到右侧栏
             $('.side_list').append(right_nav);
             // 钉住
@@ -84,7 +85,7 @@ $(function(){
             // 平滑滚动
             $('.right_nav_item').click(function () {
                 $('html, body').animate({
-                    scrollTop: $($.attr(this, 'href')).offset().top
+                    scrollTop: $($.attr(this, 'href')).offset().top - 30
                 }, 500);
                 return false;
             });
@@ -100,6 +101,22 @@ $(function(){
     setTimeout(function(){
         $('#pop_ad,.wrap-picture-show-gw,.action-from-gw,.section-service-w').remove();
     }, 1000)
+
+    // 侧栏目录
+    var fix_content = '<div class="fix_content"><svg t="1589346300333" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6064" width="30" height="30"><path d="M436.555 310.079l426.325 0c37.251 0 67.397-30.198 67.397-67.424s-30.146-67.409-67.397-67.409l-426.325 0c-37.234 0-67.417 30.184-67.417 67.409s30.184 67.424 67.417 67.424zM240.37 175.246l-80.854 0c-37.24 0-67.424 30.184-67.424 67.409s30.184 67.424 67.424 67.424l80.854 0c37.248 0 67.432-30.198 67.432-67.424s-30.184-67.409-67.432-67.409zM862.88 444.912l-426.325 0c-37.234 0-67.417 30.184-67.417 67.409 0 37.228 30.184 67.424 67.417 67.424l426.325 0c37.251 0 67.397-30.196 67.397-67.424 0-37.225-30.147-67.409-67.397-67.409zM240.37 444.912l-80.854 0c-37.24 0-67.424 30.184-67.424 67.409 0 37.228 30.184 67.424 67.424 67.424l80.854 0c37.248 0 67.432-30.196 67.432-67.424 0-37.225-30.184-67.409-67.432-67.409zM862.88 714.565l-426.325 0c-37.234 0-67.417 30.195-67.417 67.416 0 37.234 30.184 67.423 67.417 67.423l426.325 0c37.251 0 67.397-30.189 67.397-67.423 0-37.222-30.147-67.416-67.397-67.416zM240.37 714.565l-80.854 0c-37.24 0-67.424 30.195-67.424 67.416 0 37.234 30.184 67.423 67.424 67.423l80.854 0c37.248 0 67.432-30.189 67.432-67.423 0-37.222-30.184-67.416-67.432-67.416z" p-id="6065" fill="#cdcdcd"></path></svg></div><div class="fix_content_html"></div>';
+    $("body").append(fix_content);
+    $('.fix_content').click(function () {
+        $('.fix_content_html').toggle(300);
+    });
+
+    $(window).scroll(function(){
+        if($(document).scrollTop() > 400){
+            $('.fix_content').fadeIn();
+        }
+        if($(document).scrollTop() < 400){
+            $('.fix_content').fadeOut();
+        }
+    })
 
     // 图片预览
     $('#blog_body img').each(function(){
@@ -179,4 +196,5 @@ $(function(){
             document.getElementsByTagName('audio')[i].play();
         }
     })
+
 });
